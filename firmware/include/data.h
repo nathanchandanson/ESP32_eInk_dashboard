3797540357267819c37d2243b69757654894e2b6
NathanChdns <nathan.chandanson@ensta-paris.fr>
2024-12-15 17:27:44 +0100

    Updated the data.h file with new defines for the Google Calendar API

--- firmware/include/data.h
+++ firmware/include/data.h
@@ -5,11 +5,18 @@ This header contains all the credentials for the APIs and the network
 */
 
 /* Network info */
-#define NETWORK_SSID "ssid"
-#define NETWORK_PASSWORD "password"
+#define NETWORK_SSID
+#define NETWORK_PASSWORD
 
 /* Todoist API info */
-#define TODOIST_API_URL "url"
-#define TODOIST_API_TOKEN "token"
+#define TODOIST_API_URL
+#define TODOIST_API_TOKEN
+
+/* Google Calendar API info */
+#define CALENDAR_API_CLIENT_ID
+#define CALENDAR_API_CLIENT_SECRET 
+#define CALENDAR_API_REFRESH_TOKEN
+#define CALENDAR_API_FIRST_CALENDAR_URL
+#define CALENDAR_API_SECOND_CALENDAR_URL
 
 #endif
